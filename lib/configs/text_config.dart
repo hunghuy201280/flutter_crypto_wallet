9c0f99f11a584fc5a8a781b6c12a789dd0a1765b
Xuan Hung <nxhung16102001@gmail.com>
2022-01-22 14:59:50 +0700

    update - text config

--- lib/configs/text_config.dart
+++ lib/configs/text_config.dart
@@ -4,17 +4,67 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'color_config.dart';
 
 class TextConfigs {
-  static const kFontFamily = "DESIGN_FONT";
-  static final kText16_1 = TextStyle(
-    fontSize: 16.sp,
+  static const kFontFamily = "Nunito";
+  static final kHeader1_9 = TextStyle(
+    fontSize: 39.sp,
+    fontWeight: FontWeight.w800,
     fontFamily: kFontFamily,
-    color: AppColors.kColor1,
+    color: AppColors.kColor9,
   );
 
-  static final kText16Bold_1 = TextStyle(
+  static final kHeader2_9 = TextStyle(
+    fontSize: 31.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w800,
+    color: AppColors.kColor9,
+  );
+
+  static final kHeader3_9 = TextStyle(
+    fontSize: 25.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w800,
+    color: AppColors.kColor9,
+  );
+
+  static final kHeader4_9 = TextStyle(
+    fontSize: 20.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w800,
+    color: AppColors.kColor9,
+  );
+
+  static final kSubtitle_9 = TextStyle(
+    fontSize: 20.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w600,
+    color: AppColors.kColor9,
+  );
+
+  static final kLabel_9 = TextStyle(
     fontSize: 16.sp,
     fontFamily: kFontFamily,
-    fontWeight: FontWeight.bold,
-    color: AppColors.kColor1,
+    fontWeight: FontWeight.w600,
+    color: AppColors.kColor9,
+  );
+
+  static final kBody1_9 = TextStyle(
+    fontSize: 20.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w300,
+    color: AppColors.kColor9,
+  );
+
+  static final kBody2_9 = TextStyle(
+    fontSize: 16.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w300,
+    color: AppColors.kColor9,
+  );
+
+  static final kCaption_9 = TextStyle(
+    fontSize: 13.sp,
+    fontFamily: kFontFamily,
+    fontWeight: FontWeight.w300,
+    color: AppColors.kColor9,
   );
 }
