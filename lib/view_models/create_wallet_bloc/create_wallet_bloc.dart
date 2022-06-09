650cbbc36708922f61c869a3cca52a060b717e91
nxhung.hit <nxhung16102001@gmail.com>
2022-06-09 22:32:12 +0700

    fix - merge

--- lib/view_models/create_wallet_bloc/create_wallet_bloc.dart
+++ lib/view_models/create_wallet_bloc/create_wallet_bloc.dart
@@ -48,9 +48,6 @@ class CreateWalletBloc extends Bloc<CreateWalletEvent, CreateWalletState> {
         final result = await _remoteProvider.createWallet();
         if (result.error) throw result.message;
         final walletDetail = result.result!;
-        await _localProvider.addWallet(wallet: walletDetail.wallet);
-        await _localProvider.saveSelectedWallet(
-            selectedWallet: walletDetail.wallet);
         emit(
           state.copyWith(
             mnemonic: walletDetail.mnemonic,
